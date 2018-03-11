fe51d3ef5dd62d91a01134b96eae2204fd80b5bf
shy_azusa <shy.azusa@gmail.com>
2018-03-11 10:55:09 +0900

    cosme

--- app.rb
+++ app.rb
@@ -5,63 +5,100 @@ require 'RMagick'
 
 get '/' do
   @title = 'heroine_filter'
+  @fonts_path = fonts_list
   slim :index
 end
 
 post '/upload' do
   @title = 'upload'
   if params[:photo]
-    save_path = "./public/images/#{params[:photo][:filename]}"
-    File.open(save_path, 'wb') do |f|
+    image_path = "./public/images/#{params[:photo][:filename]}"
+    File.open(image_path, 'wb') do |f|
       p params[:photo][:tempfile]
       f.write params[:photo][:tempfile].read
-      @mes = 'アップロード成功'
     end
-    enchar save_path
+    addwindow image_path
+    enchar image_path, params[:message].to_s, params[:font].to_s, params[:pointsize].to_i
+    @mes = 'アップロード成功'
   else
     @mes = 'アップロード失敗'
   end
-  slim :upload
   redirect 'images'
 end
 
 get '/images' do
-  images_name = Dir.glob("./public/images/*")
+  @title = 'local images'
+  images_name = Dir.glob('./public/images/*')
   @images_path = []
+  images_name.sort!
   images_name.each do |image|
-    image_path = image.gsub("public/", "./")
-    @images_path << image_path
+    @images_path << image.gsub('public/', './')
   end
   slim :images
 end
 
 helpers do
-  def enchar image_path
+  def resize(image_path, height, width)
     image_file_name = File.basename(image_path)
     img = Magick::ImageList.new(image_path)
-    scaled_img = img.scale(300, 300)
+    new_img = img.scale(height, width)
+    new_img.write("public/images/resize_#{image_file_name}")
+    new_img.destroy!
+  end
 
-    font = "851tegaki_zatsu_normal_0883.ttf"
+  def addwindow(image_path)
+    image_file_name = File.basename(image_path)
+    img = Magick::ImageList.new(image_path)
+    start = img.rows
+    height = (img.rows / 3) * 2
+    width = img.columns
+
     draw = Magick::Draw.new
+    draw.fill('#428b0960')
+    draw.rectangle(0, start, width, height)
+    draw.draw(img)
+    img.write("public/images/#{image_file_name}")
+    img.destroy!
+  end
 
-    draw.annotate(scaled_img, 0, 0, 5, 5, 'LGTM') do
+  def enchar(image_path, char, font, pointsize)
+    image_file_name = File.basename(image_path)
+    img = Magick::ImageList.new(image_path)
+    start = img.rows
+    height = (img.rows / 3) * 2
+    width = img.columns
+    fill = '#A6126A'
+
+    @fonts_path = fonts_list
+    draw = Magick::Draw.new
+    draw.annotate(img, 0, 0, 5, height, char) do
       self.font = font
-      self.fill = '#428b09'
+      self.fill = fill
+      self.pointsize = pointsize
       self.stroke = 'white'
       self.stroke_width = 4
-      self.pointsize = 30
       self.gravity = Magick::NorthWestGravity
     end
 
-    draw.annotate(scaled_img, 0, 0, 5, 5, 'LGTM') do
+    draw.annotate(img, 0, 0, 5, height, char) do
       self.font = font
-      self.fill = '#428b09'
+      self.fill = fill
+      self.pointsize = pointsize
       self.stroke = 'transparent'
-      self.pointsize = 30
       self.gravity = Magick::NorthWestGravity
     end
 
-    scaled_img.write("public/images/#{image_file_name}")
-    scaled_img.destroy!
+    img.write("public/images/#{image_file_name}")
+    img.destroy!
+  end
+
+  def fonts_list
+    fonts_name = Dir.glob('./public/fonts/*')
+    @fonts_path = []
+    fonts_name.sort!
+    fonts_name.each do |font|
+      @fonts_path << font
+    end
+    @fonts_path
   end
 end
